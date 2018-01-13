71d2dcdb6f0a3408a73e58566f482e437dc02f16
griffin <griffin@zeoalliance.com>
2018-01-13 15:36:51 +0200

    13.01_mobile-version/1

--- script.js
+++ script.js
@@ -24,80 +24,95 @@ var data = {
     "response": [
     {
         "photo": "12",
-        "rating": "ТОП",
+        "rating": "<span class='profile-rating'>" + "ТОП" + "</span>",
         "name": "Кристиночка, 28",
         "location": "Санкт-Петербург, Россия",
         "url": "src/dating_big_eight.png",
-        "href": "#"
+        "href": "#",
+        "status":" top online-now new ",
+        "indicator": " online-indicator "
     },
     {
         "photo": "3",
-        "rating": "ТОП",
+        "rating": "<span class='profile-rating'>" + "ТОП" + "</span>",
         "name": "Диана, 20",
         "location": "Самара, Россия",
         "url": "src/dating_big_four.png",
-        "href": "#"
+        "href": "#",
+        "status":" top online-now new ",
+        "indicator": " online-indicator "
     },
     {
         "photo": "145",
-        "rating": "ТОП",
+        "rating": "<span class='profile-rating'>" + "ТОП" + "</span>",
         "name": "Аня, 18",
         "location": "Ростов-на-Дону, Россия",
         "url": "src/dating_big_three.png",
-        "href": "#"
+        "href": "#",
+        "status":" top online-now new ",
+        "indicator": " online-indicator "
     },
 
         {
             "photo": "17",
-            "rating": "ТОП",
+            "rating": "",
             "name": "Анжелика, 27",
             "location": "Чебоксары, Росси",
             "url": "src/dating_big_one.png",
-            "href": "#"
+            "href": "#",
+            "status":" new ",
+            "indicator": ""
         },
         {
             "photo": "12",
-            "rating": "ТОП",
+            "rating": "",
             "name": "Лариса, 32",
             "location": "Москва, Россия",
             "url": "src/dating_big_seven.png",
-            "href": "#"
+            "href": "#",
+            "status":" new online-now ",
+            "indicator": " online-indicator "
         },
         {
             "photo": "3",
-            "rating": "ТОП",
             "name": "Настя, 21",
+            "rating": "",
             "location": "Зеленоград, Россия",
             "url": "src/dating_big_five.png",
-            "href": "#"
+            "href": "#",
+            "status":" new ",
+            "indicator": ""
         },
         {
             "photo": "4",
-            "rating": "ТОП",
             "name": "Евгения, 30",
+            "rating": "",
             "location": "Ростов-на-Дону, Россия",
             "url": "src/dating_big_two.png",
-            "href": "#"
+            "href": "#",
+            "status":" new ",
+            "indicator": ""
         },
         {
             "photo": "28",
-            "rating": "ТОП",
             "name": "Ольга, 25",
+            "rating": "",
             "location": "Казань, Россия",
             "url": "src/dating_big_six.png",
-            "href": "#"
+            "href": "#",
+            "status":" new ",
+            "indicator": ""
         }
   ]
 }
 
 for (var i = 0; i < data.response.length; i++) {
-    $(slide).append("<a href='" + data.response[i].href + "'>" + "<div class='slider__slide js-slide'>" +
-        "<div class='slider__slide_profile'>" +
+    $(slide).append("<div class='slider__slide_profile" + data.response[i].status + "'>" +
         "<div class='absolute'>" +
         "<span class='profile-photos'>" + "<img src='src/dating-camera-icon.png' class='profile-photos__icon'>" + "<p class='profile-photos__number'>" + data.response[i].photo + "</p>"+ "</span>" +
-    "<span class='profile-rating'>" + "ТОП" + "</span>" +
+     data.response[i].rating +
         "</div>" +
-        "<div class='slider__slide_profile_photo'>" + "<img src='" + data.response[i].url +"'>" + "</div>" +
+        "<a href='" + data.response[i].href + "'>" + "<div class='slider__slide_profile_photo'>" + "<img src='" + data.response[i].url +"'>" + "</div>" + "</a>" +
         "<div class='slider__slide_profile_action'>" +
         "<div class='favorite'>" +
         "<div class='favorite__content'>" +
@@ -113,9 +128,29 @@ for (var i = 0; i < data.response.length; i++) {
         "</div>" +
         "</div>" +
         "<div class='slider__slide_profile_details'>" +
-        "<p class='name-age'>" + data.response[i].name + "</p>" +
+        "<p class='name-age'>" + data.response[i].name + "</p>" + "<span class='" + data.response[i].indicator + "'></span>" +
     "<p class='city-country'>" + data.response[i].location + "</p>" +
     "</div>" +
-    "</div>" +
-    "</div>" + "</a>");
+    "</div>");
 }
+
+$(".js-option").click(function (e) {
+    target = e.target;
+    $(".js-option-button").removeClass("active-option-button");
+    $(target).addClass("active-option-button");
+});
+
+
+$(".js-option-button-new").click(function () {
+ $(".new").css("display", "inline-block");
+})
+
+$(".js-option-button-popular").click(function () {
+    $(".new").css("display", "none");
+    $(".top").css("display", "inline-block");
+})
+
+$(".js-option-button-online").click(function () {
+    $(".new").css("display", "none");
+    $(".online-now").css("display", "inline-block");
+})
